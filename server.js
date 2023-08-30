4a849323716aac93d1ca1865dfbf59eac238db37
Abhishek Raut <102864290+abhishekraut1@users.noreply.github.com>
2023-08-30 19:48:51 +0530

    Initial Commit.

---
+++ server.js
@@ -0,0 +1,22 @@
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv"
+import routes from "./routes/ToDoRoute.js";
+import cors from "cors" 
+ 
+dotenv.config();
+
+const app = express();
+const PORT = process.env.port || 5000; 
+
+app.use(express.json())
+app.use(cors())
+
+mongoose 
+    .connect(process.env.MONGODB_URL)
+    .then(()=> console.log('connected to mongodb...'))
+    .catch((err)=> console.log(err))
+
+app.use(routes);
+
+app.listen(PORT,()=> console.log(`Listening to port ${PORT}`))
